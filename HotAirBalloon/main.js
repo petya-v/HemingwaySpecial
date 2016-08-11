eb41fe7fdbb6002c37b895722209155c10e46470
v4ss1l <vasil.cc@gmail.com>
2016-08-11 22:23:00 +0300

    Can play now with spaceBar and arrowUp

--- HotAirBalloon/main.js
+++ HotAirBalloon/main.js
@@ -60,7 +60,7 @@
     isStop = false;
 
     document.body.addEventListener("keydown", function (e) {
-        if (e.keyCode === 38) {
+        if (e.keyCode === 38 || e.keyCode === 32) {
             balloon.moveUp(physics.reverseSpeedIndex, physics.speedUp);
             balloon.drawFire();
         }
